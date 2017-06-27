6a52d38e1ef1d105dd89fde850c7867d70ca6262
Adrian Daras <getpingui@gmail.com>
2017-06-27 08:41:16 +0200

    Add `filterEmpty`.

---
+++ src/filterEmpty.js
@@ -0,0 +1,25 @@
+/**
+ * Filters the passed value and returns `undefined` if either:
+ * – An [array] or an {object} is empty.
+ * – `null` has been passed.
+ * – `NaN` has been passed.
+ *
+ * @flow
+ * @param {*} a – The passed data.
+ * @return {*|undefined} – Either `undefined` or the passed data.
+ */
+
+function filterEmpty(a: mixed): mixed {
+  if (typeof a === "object") {
+    // typeof null => "object"
+    if (a === null) return undefined;
+
+    return Object.keys(a).length > 0 ? a : undefined;
+  }
+  
+  if (Number.isNaN(a)) return undefined;
+
+  return a;
+}
+
+module.exports = filterEmpty;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const filterEmpty = require("./filterEmpty");
+
+module.exports = {
+  filterEmpty,
+};
