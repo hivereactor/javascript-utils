1955e145d294865c5e64d73ed31b2e1342e33bd1
Adrian Daras <getpingui@gmail.com>
2017-07-16 11:48:43 +0200

    Fix tests

--- test/numberIsNaN.test.js
+++ test/numberIsNaN.test.js
@@ -2,10 +2,10 @@ const test = require("ava").default;
 const { numberIsNaN } = require("../lib/cjs");
 
 // numbers
-test("0 => false", () => t.is(numberIsNaN(0), false));
-test("1 => false", () => t.is(numberIsNaN(1), false));
-test("NaN => true", () => t.is(numberIsNaN(NaN), true));
+test("0 => false", (t) => t.is(numberIsNaN(0), false));
+test("1 => false", (t) => t.is(numberIsNaN(1), false));
+test("NaN => true", (t) => t.is(numberIsNaN(NaN), true));
 
 // strings
-test(`"" => false`, () => t.is(numberIsNaN(""), false));
-test(`"a" => false`, () => t.is(numberIsNaN("a"), false));
+test(`"" => false`, (t) => t.is(numberIsNaN(""), false));
+test(`"a" => false`, (t) => t.is(numberIsNaN("a"), false));
